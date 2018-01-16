f458a16db03255f6fe85aaa2099429d0878c2e80
Stefan Adams <stefan@adams.fm>
2018-01-16 07:41:13 +0000

    first commit

---
+++ lib/Mojo/Autotask.pm
@@ -0,0 +1,38 @@
+package Mojo::Autotask;
+use Mojo::Base -base;
+
+use Carp 'croak';
+use Mojo::Pg 4.0;
+use Mojo::Log;
+use Mojo::Date;
+use Mojo::JSON 'j';
+use Mojo::Util 'encode';
+use Mojo::Loader 'load_class';
+use Mojo::Collection;
+
+use Scalar::Util 'weaken';
+
+use Mojo::Autotask::WebService;
+
+has 'backend';
+has log => sub { Mojo::Log->new };
+has webservice => sub { Mojo::Autotask::WebService->new(@_) };
+
+sub new {
+  my $self = shift->SUPER::new(@_);
+
+  my $backend = shift;
+  my $class = 'Mojo::Autotask::Backend::' . $backend;
+  my $e     = load_class $class;
+
+  if ( $e ) {
+    unshift @_, $backend;
+  } else {
+    $self->backend($class->new(@_));
+    weaken $self->backend->autotask($self)->{autotask};
+  }
+
+  return $self;
+}
+
+1;
