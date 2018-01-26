89320445605a1ef8b53c76a9470d9e52aecfbdba
Stefan Adams <stefan@adams.fm>
2018-01-26 13:59:19 +0000

    handle entire api via AUTOLOAD and validate

--- lib/Mojo/Autotask/ExecuteCommand.pm
+++ lib/Mojo/Autotask/ExecuteCommand.pm
@@ -1,13 +1,63 @@
 package Mojo::Autotask::ExecuteCommand;
 use Mojo::Base -base;
 
-has _open_ticket_detail => 'https://ww5.autotask.net/Autotask/AutotaskExtend/ExecuteCommand.aspx?Code=OpenTicketDetail&TicketNumber=%s&AccountID=%s';
+use Mojo::URL;
+use Mojo::Util 'camelize';
+use Mojolicious::Validator;
 
-sub open_ticket_detail { sprintf shift->_open_ticket_detail, _a2h(@_) }
+use Carp;
+use Scalar::Util;
 
-sub _a2h {
-  local %_ = @_;
-  map { $_ => $_{$_} } keys %_;
+has baseurl => sub { Mojo::URL->new('https://ww5.autotask.net/AutotaskAutotaskExtend/ExecuteCommand.aspx') };
+
+sub AUTOLOAD {
+  my ($self, @args) = @_;
+
+  my ($package, $method) = our $AUTOLOAD =~ /^(.+)::(.+)$/;
+  Carp::croak "Undefined subroutine &${package}::$method called"
+    unless Scalar::Util::blessed $self && $self->isa(__PACKAGE__);
+
+  my $validator  = Mojolicious::Validator->new;
+  my $validation = $validator->validation;
+
+  $validation->input({Code => camelize($method), @args});
+  $validation->optional('TicketID')->is_valid
+    || $validation->optional('TicketNumber')->is_valid
+    || $validation->optional('GlobalTaskID')->is_valid
+    if $validation->required('Code')->like(qr/OpenTicketDetail/);
+  # NewTicket: Phone, AccountID, AccountName, GlobalTaskID, InstalledProductID
+  # OpenTicketDetail: TicketID, TicketNumber, GlobalTaskID
+  # NewAccount:
+  # OpenAccount: Phone, AccountID, AccountName
+  # EditAccount: Phone, AccountiD
+  # NewAccountNote: AccountID
+  # NewContact: Phone, AccountID
+  # EditContact: ContactID, Email, FirstName, LastName
+  # OpenContact: Email, ContactID
+  # NewInstalledProduct: Phone, AccountID
+  # EditInstalledProduct: InstalledProductID
+  # OpenTicketTime: TicketID
+  # EditTimeEntry: WorkEntryID
+  # OpenAppointment: AppointmentID
+  # OpenContract: ContractID
+  # IPDW: wizard
+  # OpenKBArticle: ID
+  # OpenOpportunity: OpportunityID
+  # OpenProject: ProjectID
+  # OpenSalesOrder: SalesOrderID
+  # OpenServiceCall: ServiceCallID
+  # OpenTaskDetail: TaskID
+  # TimeOffRequest: ResourceID, ApproverID, Tier
+  # OpenToDo: ToDoID
+  # https://ww5.autotask.net/help/Content/AdminSetup/2ExtensionsIntegrations/APIs/ExecuteCommand/UsingExecuteCommandAPI.htm
+
+  return $self->baseurl->query($validation->input);# unless $validation->has_error;
 }
 
 1;
+
+=head1 NAME
+
+Mojo::Autotask::ExecuteCommand - Mojo interface to the Autotask ExecuteCommand API
+
+=cut
